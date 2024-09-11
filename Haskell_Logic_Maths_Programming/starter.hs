4fb9cf813cb0a3d0a6247dae1d507713c093f965
exrnd <ever.ex.rnd@gmail.com>
2024-09-11 23:30:16 +0300

    second commit

---
+++ Haskell_Logic_Maths_Programming/starter.hs
@@ -0,0 +1,24 @@
+-- 2^16
+
+-- (2+3)^4
+
+-- divides d n = rem n d == 0
+
+-- divides 5 7 -> False
+
+-- divides 5 30 -> True 
+
+
+-- See Definition -- Least Divisor 
+--ldf k n | divides k n = k
+--        | k^2 > n = n
+--        | otherwise = ldf (k+1) n
+
+
+-- divides :: Integer -> Integer -> Bool
+
+-- divides d n = rem n d == 0
+
+
+
+
